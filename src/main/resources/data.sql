986e7f16d703c63805bb7c2d4b0a097e9a9065fe
Kamil <kamil.marusia@upsanok.onmicrosoft.com>
2026-05-01 14:02:34 +0200

    h2-database

---
+++ src/main/resources/data.sql
@@ -0,0 +1,20 @@
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS flowers;
+
+CREATE TABLE flowers (
+    flowers_id INT AUTO_INCREMENT PRIMARY KEY,
+    flowers_name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE users (
+    users_id INT AUTO_INCREMENT PRIMARY KEY,
+    users_name VARCHAR(255) NOT NULL,
+    users_favourite_flower_id INT
+);
+
+INSERT INTO flowers (flowers_name) VALUES
+    ('Bratek'),
+    ('Stokrotka'),
+    ('Pierwiosnek'),
+    ('Roza'),
+    ('Lilia');
